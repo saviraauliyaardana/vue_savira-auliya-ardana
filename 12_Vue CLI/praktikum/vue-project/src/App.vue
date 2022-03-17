8e8a042b7fa91ee5fc51def81d75a24b59975bf1
Newpir <savirauliya10@gmail.com>
2022-03-17 15:46:56 +0700

    update screenshots

---
+++ 12_Vue CLI/praktikum/vue-project/src/App.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div class="bungkus">
+    <div class="comp">
+      <ol>
+        <li v-for="(todo, index) in todos" todo in todos :key="index">
+          {{ todo.text }}
+        </li>
+      </ol>
+      <input type="text" v-model="newTodo" />
+      <button v-on:click="addTodo">Tambahkan</button>
+      <p v-if="todos.length == 4">Hebat!</p>
+    </div>
+  </div>
+</template>
+<script>
+// import { defineComponent } from "@vue/composition-api";
+
+export default {
+  setup() {},
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      var newItem = this.newTodo.trim();
+      if (newItem) {
+        this.todos.push({
+          text: newItem,
+        });
+        this.newTodo = "";
+      }
+    },
+  },
+};
+</script>
+<style >
+.bungkus {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
